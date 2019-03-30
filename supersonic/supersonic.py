d684559d6c2181a752dc9c8e62d2eb391671d926
Jill Wung <b06901087@ntu.edu.tw>
2019-03-30 15:10:39 +0800

    three supersonic

--- supersonic/supersonic.py
+++ supersonic/supersonic.py
@@ -1,33 +1,49 @@
 import RPi.GPIO as GPIO
 import time
-trigger_pin = 23
-echo_pin = 24
+trigger_pin_boxopen = 2
+echo_pin_boxopen = 3
+trigger_pin_up = 14
+echo_pin_up = 15
+trigger_pin_down = 17
+echo_pin_down = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(trigger_pin, GPIO.OUT)
-GPIO.setup(echo_pin, GPIO.IN)
+GPIO.setup(trigger_pin_boxopen, GPIO.OUT)
+GPIO.setup(echo_pin_boxopen, GPIO.IN)
+GPIO.setup(trigger_pin_up, GPIO.OUT)
+GPIO.setup(echo_pin_up, GPIO.IN)
+GPIO.setup(trigger_pin_down, GPIO.OUT)
+GPIO.setup(echo_pin_down, GPIO.IN)
 
-def send_trigger_pulse():
+def send_trigger_pulse(trigger_pin):
     GPIO.output(trigger_pin, True)
     time.sleep(0.001)
     GPIO.output(trigger_pin, False)
 
-def wait_for_echo(value, timeout):
+def wait_for_echo(value, timeout, echo_pin):
     count = timeout
     while GPIO.input(echo_pin) != value and count > 0:
         count = count - 1
 
-def get_distance():
-    send_trigger_pulse()
-    wait_for_echo(True, 5000)
+def get_distance(trigger_pin, echo_pin):
+    send_trigger_pulse(trigger_pin)
+    wait_for_echo(True, 5000, echo_pin)
     start = time.time()
-    wait_for_echo(False, 5000)
+    wait_for_echo(False, 5000, echo_pin)
     finish = time.time()
     pulse_len = finish - start
     distance_cm = pulse_len * 340 *100 /2
     distance_in = distance_cm / 2.5
-    return (distance_cm, distance_in)
+    return distance_cm
 
 while True:
-    print("cm=%f\tinches=%f" % get_distance())
+    dis_boxopen = get_distance(trigger_pin_boxopen,echo_pin_boxopen)
+    dis_up = get_distance(trigger_pin_up,echo_pin_up)
+    dis_down = get_distance(trigger_pin_down,echo_pin_down)
+    if dis_boxopen > 30:
+        print("box is open!")
+        print(get_distance(trigger_pin_up,echo_pin_up))
+        print(get_distance(trigger_pin_down,echo_pin_down))
+    else:
+        print("box is closed")
     time.sleep(1)
