f1cec6a1c20ce040c77e3c9afce61c688141baab
Ubuntu <ubuntu@ip-172-31-45-48.ap-northeast-1.compute.internal>
2019-03-30 12:18:06 +0000

    finshed request response

--- app_engine/hello.py
+++ app_engine/hello.py
@@ -1,8 +1,31 @@
+from flask import request
 from flask import Flask
+import json
+from flask import jsonify
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/Hello', methods = ['GET','POST'])
+def response():
+    a = request.args.get('a')
+    print(a)
+    x = {"messages": [
+    {
+      "type": "text",
+      "text": "歡迎使用最聰明的聊天機器人平台 —— YOCTOL.AI"
+    },
+    {
+      "type": "text",
+      "text": "您打算創建怎樣的機器人呢？"
+    }
+  ]
+}
+    return "<h1>hahah</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
