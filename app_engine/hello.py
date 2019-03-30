f9686192a5d74ffaeb6c255ac23ec4c2ee70aab5
LiverMalfunction <b06901096@g.ntu.edu.tw>
2019-03-30 23:01:04 +0800

    appengine

--- app_engine/hello.py
+++ app_engine/hello.py
@@ -1,9 +1,11 @@
 from flask import request, Flask, jsonify
 import json
 
+to_bring_list = {}
+brougth_list = {}
 app = Flask(__name__)
 
-@app.route('/Hello', methods = ['GET','POST'])
+@app.route('/ChatBot', methods = ['GET','POST'])
 def response():
     a = request.args.get('a')
     print(a)
@@ -22,8 +24,8 @@ def response():
 
 @app.route('/RPi', methods = ['GET','POST'])
 def RPi_response():
-    b = request.args.get('b')
-    print('b = ', b)
+    b = request.get_json()
+    print(b)
     return "<h1>RPi<h1>"
 
 if __name__ == "__main__":
